3bed6c381f2e31f89f3327e6c1fa46070d991324
VS <solonin.vadim@mail.ru>
2023-10-15 11:14:01 +0300

    try to fix jenkins build

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,6 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import pages.RegistrationPage;
 import pages.TextBoxPage;
@@ -20,11 +18,6 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
-    }
-
-    @AfterEach
-    void afterEach() {
-        Selenide.closeWebDriver();
     }
 
 }
