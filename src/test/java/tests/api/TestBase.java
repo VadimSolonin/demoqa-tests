6dd3e46211414e69dd7ae117fc0a0182bffe17bc
VS <solonin.vadim@mail.ru>
2023-11-25 14:20:21 +0300

    add webconfig

--- src/test/java/tests/api/TestBase.java
+++ src/test/java/tests/api/TestBase.java
@@ -1,7 +1,9 @@
 package tests.api;
 
 import com.codeborne.selenide.Configuration;
+import config.WebConfig;
 import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 import tests.api.models.LoginRequestModel;
 
@@ -9,8 +11,11 @@ public class TestBase {
     LoginRequestModel loginRequestModel = new LoginRequestModel(TestData.LOGIN, TestData.PASSWORD);
     @BeforeAll
     static void beforeAll() {
+        WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
+
         RestAssured.baseURI = "https://demoqa.com/";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = webConfig.getRemoteUrl();
     }
 }
